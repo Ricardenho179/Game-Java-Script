c95d2ac1b983e5fc9c5b03b2999f7b33597f5029
Jonatas <djonatas31@gmail.com>
2023-04-28 02:02:23 -0300

    Arrumando identação da função startGame

--- js/componentes.js
+++ js/componentes.js
@@ -1,30 +1,30 @@
 function startGame() {
-    myGameArea.start();
-    myGamePiece = new component(15, 15, "black", 30, 120);
-    GamePiece1 = new component(15, 15, "grey", 30, 20);
-    GamePiece2 = new component(15, 15, "green", 30, 70);    
-    GamePiece3 = new component(15, 15, "purple", 30, 320);
-    GamePiece4 = new componentAutomatic(15, 15, "blue",37, 170);
-    GamePiece5 = new componentAutomatic(15, 15, "red", 37, 220);
-    GamePiece6 = new componentAutomatic(15,15,"orange", 37, 270)
-    GamePieceG = new component(15, 15, "#191970", 1035, 660);
-    obstacle  = new component(10, 700, "yellow", 1070, 0);
-    obstacle2  = new component(1100, 10, "yellow", 0, 0);
-    obstacle3  = new component(1100, 10, "yellow", 0, 690);
-    obstacle4  = new component(10, 700, "yellow", 0, 0);
-    obstacle5 = new component(10, 150, "yellow", 70, 0);
-    obstacle6 = new component(10, 300, "yellow", 900, 360);
-    obstacle7 = new component(300, 10, "yellow", 600, 400);
-    obstacle8 = new component(620, 10, "yellow", 250, 550);
-    obstacle9 = new component(400, 10, "yellow", 500, 510);
-    obstacleG = new component(150, 10, "#191970", 870, 550);
-    blackObstacle = new component(10,200, "black", 310,0);
-    greyObstacle = new component(10, 430, "grey", 770,35);
-    greenObstacle = new component(570, 10, "green", 0,400);
-    blueObstacle = new component(10, 180 ,"blue", 110, 50);
-    orangeObstacle = new component(210, 10, "orange", 0, 550);
-    redObstacle = new component(1030,10,"red", 0,230);
-    area = new component(30, 30, "#11b06a", 860, 470);
+  myGameArea.start();
+  myGamePiece = new component(15, 15, "black", 30, 120);
+  GamePiece1 = new component(15, 15, "grey", 30, 20);
+  GamePiece2 = new component(15, 15, "green", 30, 70);    
+  GamePiece3 = new component(15, 15, "purple", 30, 320);
+  GamePiece4 = new componentAutomatic(15, 15, "blue",37, 170);
+  GamePiece5 = new componentAutomatic(15, 15, "red", 37, 220);
+  GamePiece6 = new componentAutomatic(15,15,"orange", 37, 270)
+  GamePieceG = new component(15, 15, "#191970", 1035, 660);
+  obstacle  = new component(10, 700, "yellow", 1070, 0);
+  obstacle2  = new component(1100, 10, "yellow", 0, 0);
+  obstacle3  = new component(1100, 10, "yellow", 0, 690);
+  obstacle4  = new component(10, 700, "yellow", 0, 0);
+  obstacle5 = new component(10, 150, "yellow", 70, 0);
+  obstacle6 = new component(10, 300, "yellow", 900, 360);
+  obstacle7 = new component(300, 10, "yellow", 600, 400);
+  obstacle8 = new component(620, 10, "yellow", 250, 550);
+  obstacle9 = new component(400, 10, "yellow", 500, 510);
+  obstacleG = new component(150, 10, "#191970", 870, 550);
+  blackObstacle = new component(10,200, "black", 310,0);
+  greyObstacle = new component(10, 430, "grey", 770,35);
+  greenObstacle = new component(570, 10, "green", 0,400);
+  blueObstacle = new component(10, 180 ,"blue", 110, 50);
+  orangeObstacle = new component(210, 10, "orange", 0, 550);
+  redObstacle = new component(1030,10,"red", 0,230);
+  area = new component(30, 30, "#11b06a", 860, 470);
 }
 function componentAutomatic(width, height, color, x, y, type) {
   this.type = type;
@@ -137,53 +137,53 @@ function componentAutomatic(width, height, color, x, y, type) {
   }
 }
 function component(width, height, color, x, y) {
-    this.width = width;
-    this.height = height;
-    this.speedX = 0;
-    this.speedY = 0;
-    this.x = x;
-    this.y = y;
-    this.deleted = false    
-    this.update = function() {
-      if (!this.deleted){
-        ctx = myGameArea.context;
-        ctx.fillStyle = color;
-        ctx.fillRect(this.x, this.y, this.width, this.height);
-      }
+  this.width = width;
+  this.height = height;
+  this.speedX = 0;
+  this.speedY = 0;
+  this.x = x;
+  this.y = y;
+  this.deleted = false    
+  this.update = function() {
+    if (!this.deleted){
+      ctx = myGameArea.context;
+      ctx.fillStyle = color;
+      ctx.fillRect(this.x, this.y, this.width, this.height);
     }
-    this.newPos = function() {
-        this.x += this.speedX;
-        this.y += this.speedY;
+  }
+  this.newPos = function() {
+      this.x += this.speedX;
+      this.y += this.speedY;
+  }
+  this.crashWith = function(otherobj) {
+    var myleft = this.x;
+    var myright = this.x + (this.width);
+    var mytop = this.y;
+    var mybottom = this.y + (this.height);
+    var otherleft = otherobj.x;
+    var otherright = otherobj.x + (otherobj.width);
+    var othertop = otherobj.y;
+    var otherbottom = otherobj.y + (otherobj.height);
+    crash = false
+    if ((mybottom < othertop) ||
+    (mytop > otherbottom) ||
+    (myright < otherleft) ||
+    (myleft > otherright)) {
+      
+    } else if(otherobj == area ) {
+      crash = true
+      return crash 
+    } else {
+      this.delete()
     }
-    this.crashWith = function(otherobj) {
-      var myleft = this.x;
-      var myright = this.x + (this.width);
-      var mytop = this.y;
-      var mybottom = this.y + (this.height);
-      var otherleft = otherobj.x;
-      var otherright = otherobj.x + (otherobj.width);
-      var othertop = otherobj.y;
-      var otherbottom = otherobj.y + (otherobj.height);
-      crash = false
-      if ((mybottom < othertop) ||
-      (mytop > otherbottom) ||
-      (myright < otherleft) ||
-      (myleft > otherright)) {
-        
-      } else if(otherobj == area ) {
-        crash = true
-        return crash 
-      } else {
-        this.delete()
-      }
-    }
-    this.delete = function() {
-      this.deleted = true
-      // ctx.clearRect(this.x, this.y, this.width, this.height);
-      this.x += this.speedX = 100000000000
-      this.y += this.speedY = 100000000000
+  }
+  this.delete = function() {
+    this.deleted = true
+    // ctx.clearRect(this.x, this.y, this.width, this.height);
+    this.x += this.speedX = 100000000000
+    this.y += this.speedY = 100000000000
 
-    }
+  }
 }
 
 
